d8051788a2637453db0932762442d88b1c7d5ce1
Marin Nicolae <nicu@studenttenant.com>
2018-06-05 22:51:31 +0300

    working

--- main.js
+++ main.js
@@ -2,25 +2,27 @@ var stations = [{
 	distance: 0,
 	name: 'S1'},
 {
-	distance: 4000,
+	distance: 40,
 	name: 'S2'},
 {
-	distance: 8000,
+	distance: 80,
 	name: 'S3'},
 {
-	distance: 12000,
+	distance: 120,
 	name: 'S4'},
 {
-	distance: 16000,
+	distance: 160,
 	name: 'S5'},
 {
-	distance: 20000,
+	distance: 200,
 	name: 'S6'},
 {
-	distance: 24000,
+	distance: 240,
 	name: 'S7'}];
 
 var trainId = 0;
+
+var typeNames = {'R': 'Regio', 'IR': 'Inter Regio'};
 
 function Train(type)
 {
@@ -37,29 +39,33 @@ function Train(type)
 	this.lastStation = stations[0];
 	this.nextStation = stations[1];
 	this.id = 'TrainId: ' + ++trainId;
-	this.eta = parseInt((this.nextStation.distance - this.distance) / this.speed);
+	this.id = trainId;
 	this.updateEta = function() {
-		this.eta--;
+		if (this.stop<1) {
+			this.eta = parseInt((this.nextStation.distance - this.distance) / this.speed);
+		}
 	}
+	this.updateEta();
 	this.update = function () {
 		if (this.stop) {
-			this.stop--;
+			this.stop-=1;
 		} else {
 			this.distance += this.speed;
 			this.updateEta();
 		}
 
 		if (this.distance > this.nextStation.distance) {
-			this.lastStation = this.nextStation;
-			$.each(stations, function( index, station ) {
-	 			if (this.lastStation.name === station.name) {
-					this.nextStation = stations[i++];
+			let last = this.lastStation = this.nextStation;
+			this.distance = this.nextStation.distance;
+			$.each(stations, function( i, station ) {
+	 			if (last.name === station.name) {
+					nextStation = stations[++i];
 				}
 			});
-			this.stop=5*60;
-		}
+			this.nextStation = nextStation;
 
-		console.log(this);
+			this.stop=5;
+		}
 
 	}
 }
@@ -79,7 +85,21 @@ $( document ).ready(function() {
 
 function update(trains) {
 	$.each(trains, function( index, train ) {
-	  train.update();
+		if (train.lastStation.name !== 'S7') {
+			train.update();
+		 	let trainVar = {
+			'eta': (train.stop ? 'Will live in: '+train.stop : 'ETA to next statison: '+(train.eta?train.eta:'-') )+' seconds',
+			'type': typeNames[train.type],
+			'status': train.stop ? 'Train is in station: '+this.lastStation.name : 'Next station: '+this.nextStation.name
+			};
+			
+			var holderSelector = $('#train'+train.id);
+			holderSelector.find('#eta').html(trainVar.eta);
+			holderSelector.find('#trainType').html(trainVar.type);
+			holderSelector.find('#status').html(trainVar.status);
+		} else {
+			holderSelector.find('#eta').html('STOPED');
+		}
 	});
 }
 
@@ -100,7 +120,8 @@ function run() {
 }
 
 function show() {
+	var template = $('#dataRow').html();
 	$.each(trains, function( index, train ) {
-	 	
+
 	});
 }
